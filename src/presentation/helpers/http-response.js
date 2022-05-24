1ab39a0054de59c9b371e76448000ad292596df9
daniel valente <daniel.araujo@eemovel.com>
2022-05-24 15:53:11 -0300

    feat: login router email validation

--- src/presentation/helpers/http-response.js
+++ src/presentation/helpers/http-response.js
@@ -1,4 +1,4 @@
-const MissingParamError = require('./missing-param-error')
+
 const UnauthorizedError = require('./unauthorized-error')
 const ServerError = require('./server-error')
 
@@ -10,10 +10,10 @@ module.exports = class HttpResponse {
     }
   }
 
-  static badRequest (paramName) {
+  static badRequest (error) {
     return {
       statusCode: 400,
-      body: new MissingParamError(paramName)
+      body: error
     }
   }
 
