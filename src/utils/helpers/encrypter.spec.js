84b8e68d12687178fa33e0af335c3055d4c1385c
daniel valente <daniel.araujo@eemovel.com>
2022-05-25 17:23:53 -0300

    feat: move encrypter to its own file

--- src/utils/helpers/encrypter.spec.js
+++ src/utils/helpers/encrypter.spec.js
@@ -1,23 +1,27 @@
 const bcrypt = require('bcrypt')
+const Encrypter = require('./encrypter')
 
-class Encrypter {
-  async compare(value, hash) {
-    const isValid = bcrypt.compare(value, hash)
-
-    return isValid
-  }
+const makeSut = () => {
+  return new Encrypter()
 }
+
 describe('Encrypter', () => {
   test('Should return true if bcrypt return true', async () => {
-    const sut = new Encrypter()
+    const sut = makeSut()
     const isValid = await sut.compare('any_value', 'hashed_value')
     expect(isValid).toBe(true)
   })
 
   test('Should return false if bcrypt return false', async () => {
-    const sut = new Encrypter()
+    const sut = makeSut()
     bcrypt.isValid = false
     const isValid = await sut.compare('any_value', 'hashed_value')
     expect(isValid).toBe(false)
+  })
+
+  test('Should return false if bcrypt return false', async () => {
+    const sut = makeSut()
+    expect(bcrypt.value).toBe('any_value')
+    expect(bcrypt.hash).toBe('hashed_value')
   })
 })
