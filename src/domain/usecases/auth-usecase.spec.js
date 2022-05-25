3d2ff9e326cdbe2c4c82acc9cd121d7e4c5d00a9
daniel valente <daniel.araujo@eemovel.com>
2022-05-25 11:32:50 -0300

    feat: email and password validation for login

--- src/domain/usecases/auth-usecase.spec.js
+++ src/domain/usecases/auth-usecase.spec.js
@@ -1,7 +1,13 @@
+const MissingParamError = require('../../utils/errors/missing-param-error')
+
 class AuthUseCase {
-  async auth (email) {
+  async auth (email, password) {
     if (!email) {
-      throw new Error()
+      throw new MissingParamError('email')
+    }
+
+    if (!password) {
+      throw new MissingParamError('password')
     }
   }
 }
@@ -10,6 +16,12 @@ describe('Auth Usecase', () => {
   test('Should throw if no email is provided', async () => {
     const sut = new AuthUseCase()
     const promise = sut.auth()
-    expect(promise).rejects.toThrow()
+    expect(promise).rejects.toThrow(new MissingParamError('email'))
+  })
+
+  test('Should throw if no password is provided', async () => {
+    const sut = new AuthUseCase()
+    const promise = sut.auth('any_email@mail.com')
+    expect(promise).rejects.toThrow(new MissingParamError('password'))
   })
 })
